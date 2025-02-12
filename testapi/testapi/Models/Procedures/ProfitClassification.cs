345e6e994b5f1161683f340ef1eb778d9bffeb44
Federico Giuliano (External) <E-federico.giuliano@msc.com>
2025-02-12 16:01:22 +0000

    Merged PR 1495: test riordinati
    test riordinati

--- testapi/testapi/Models/Procedures/ProfitClassification.cs
+++ testapi/testapi/Models/Procedures/ProfitClassification.cs
@@ -6,6 +6,6 @@
         public decimal? TotalRevenue { get; set; }
         public decimal? TotalSpent { get; set; }
         public decimal? Profit { get; set; }
-        public string? SaleMargins { get; set; }
+        public string? SaleMargins { get; set; } // profit, no profit, risky
     }
 }
