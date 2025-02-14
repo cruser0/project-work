9bb1bd20f3c4568216c79151683fe63a461eab0e
Catalin Grumeza (MSC Technology Italia) <catalin.grumeza@msc.com>
2025-02-14 16:41:12 +0100

    test win components

---
+++ testapi/Winform/Forms/testForm.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Winform.Forms
+{
+    partial class testForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.baseGridComponent = new Winform.Forms.control.BaseGridUserControl();
+            this.SuspendLayout();
+            // 
+            // baseGridComponent
+            // 
+            this.baseGridComponent.Location = new System.Drawing.Point(12, 85);
+            this.baseGridComponent.Name = "baseGridComponent";
+            this.baseGridComponent.Size = new System.Drawing.Size(1239, 533);
+            this.baseGridComponent.TabIndex = 0;
+            // 
+            // testForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 623);
+            this.Controls.Add(this.baseGridComponent);
+            this.Name = "testForm";
+            this.Text = "testForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private control.BaseGridUserControl baseGridComponent;
+    }
+}
---
+++ testapi/Winform/Forms/testForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Winform.Entities;
+using Winform.Forms.control;
+using Winform.Services;
+
+namespace Winform.Forms
+{
+    public partial class testForm : Form
+    {
+        private CustomerService _customerService;
+        public testForm()
+        {
+            _customerService = new CustomerService();
+            InitializeComponent();
+            baseGridComponent.buttonGet += MyControl_ButtonClicked;
+            baseGridComponent.dgvDoubleClick += MyControl_OpenDetails_Clicked;
+        }
+        private void MyControl_ButtonClicked(object sender, EventArgs e)
+        {
+            baseGridComponent.setDataGrid(_customerService.GetAll());
+        }
+        private void MyControl_OpenDetails_Clicked(object sender, DataGridViewCellEventArgs e)
+        {
+            if(sender is DataGridView dgv)
+            {
+                MessageBox.Show(dgv.CurrentRow.Cells[1].Value.ToString());
+            }
+            else
+            {
+                MessageBox.Show(sender.ToString());
+
+            }
+
+        }
+    }
+}
