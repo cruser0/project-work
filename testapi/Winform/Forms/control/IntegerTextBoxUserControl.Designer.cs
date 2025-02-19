d2cbdd9d88a4b618f920540692cc02d0ce94059b
Federico Giuliano (External) <E-federico.giuliano@msc.com>
2025-02-19 08:24:04 +0000

    Merged PR 1527: added numeric textbox user control
    added numeric textbox user control

---
+++ testapi/Winform/Forms/control/IntegerTextBoxUserControl.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Winform.Forms.control
+{
+    partial class IntegerTextBoxUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumericTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // NumericTxt
+            // 
+            this.NumericTxt.BackColor = System.Drawing.Color.Gainsboro;
+            this.NumericTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NumericTxt.Location = new System.Drawing.Point(0, 0);
+            this.NumericTxt.Name = "NumericTxt";
+            this.NumericTxt.Size = new System.Drawing.Size(194, 23);
+            this.NumericTxt.TabIndex = 2;
+            this.NumericTxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumericTxt_OnKeyPress);
+            // 
+            // IntegerTextBoxUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.NumericTxt);
+            this.Name = "IntegerTextBoxUserControl";
+            this.Size = new System.Drawing.Size(194, 23);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox NumericTxt;
+    }
+}
---
+++ testapi/Winform/Forms/control/IntegerTextBoxUserControl.cs
@@ -0,0 +1,25 @@
+﻿namespace Winform.Forms.control
+{
+    public partial class IntegerTextBoxUserControl : UserControl
+    {
+        public IntegerTextBoxUserControl()
+        {
+            InitializeComponent();
+            Width = NumericTxt.Width;
+            Height = NumericTxt.Height;
+        }
+
+        private void NumericTxt_OnKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        public string GetText()
+        {
+            return NumericTxt.Text;
+        }
+    }
+}
