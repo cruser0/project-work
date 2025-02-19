7174169b0beb2ac93ed8aa935401cc83b5ce6a09
Federico Giuliano (External) <E-federico.giuliano@msc.com>
2025-02-19 16:28:34 +0100

    added sup plier invoice cost form

---
+++ testapi/Winform/Forms/CreateWindow/SupplierInvoiceCostGridForm.cs
@@ -0,0 +1,52 @@
+﻿using API.Models.Filters;
+using Winform.Entities;
+using Winform.Services;
+
+namespace Winform.Forms.CreateWindow
+{
+    public partial class SupplierInvoiceCostGridForm : Form
+    {
+        SupplierInvoiceCostService _supplierInvoiceCostService;
+        public SupplierInvoiceCostGridForm()
+        {
+            _supplierInvoiceCostService = new SupplierInvoiceCostService();
+            InitializeComponent();
+
+            RightSideBar.searchBtnEvent += MyControl_ButtonClicked;
+            RightSideBar.closeBtnEvent += RightSideBar_closeBtnEvent;
+        }
+
+        private void RightSideBar_closeBtnEvent(object? sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MyControl_OpenDetails_Clicked(object? sender, DataGridViewCellEventArgs e)
+        {
+            if (sender is DataGridView dgv)
+            {
+                SupplierInvoiceDetailsForm sid = new SupplierInvoiceDetailsForm(int.Parse(dgv.CurrentRow.Cells[0].Value.ToString()));
+                sid.Show();
+            }
+            else
+            {
+                MessageBox.Show(sender.ToString());
+
+            }
+        }
+
+        private void MyControl_ButtonClicked(object? sender, EventArgs e)
+        {
+            SupplierInvoiceCostFilter filter = new SupplierInvoiceCostFilter
+            {
+                SupplierInvoiceId = !string.IsNullOrEmpty(InvoiceIDTxt.GetText()) ? int.Parse(InvoiceIDTxt.GetText()) : null,
+                Cost = !string.IsNullOrEmpty(CostTxt.GetText()) ? int.Parse(CostTxt.GetText()) : null
+            };
+
+            IEnumerable<SupplierInvoiceCost> query = _supplierInvoiceCostService.GetAll(filter);
+
+            SupplierInvoiceCostDgv.DataSource = query.ToList();
+
+        }
+    }
+}
---
+++ testapi/Winform/Services/SupplierInvoiceCostService.cs
@@ -0,0 +1,163 @@
+﻿using API.Models.Filters;
+using System.Text;
+using System.Text.Json;
+using Winform.Entities;
+
+namespace Winform.Services
+{
+    internal class SupplierInvoiceCostService : ICalls<SupplierInvoiceCost>
+    {
+        public ICollection<SupplierInvoiceCost> GetAll(SupplierInvoiceCostFilter filter)
+        {
+            ClientAPI client = new ClientAPI();
+            var queryParameters = new List<string>();
+
+            if (filter.SupplierInvoiceId != null)
+                queryParameters.Add($"SupplierInvoiceId={filter.SupplierInvoiceId}");
+
+            if (filter.Cost != null)
+                queryParameters.Add($"Cost={filter.Cost}");
+
+            if (filter.Quantity != null)
+                queryParameters.Add($"Quantity={filter.Quantity}");
+
+            if (filter.page != null)
+                queryParameters.Add($"page={filter.page}");
+
+            string queryString = queryParameters.Any() ? "?" + string.Join("&", queryParameters) : string.Empty;
+            HttpResponseMessage response = client.GetClient().GetAsync(client.GetBaseUri() + "supplier-invoice-cost" + queryString).Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                // Leggere il contenuto della risposta
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                // Deserializzare la risposta JSON in una lista di oggetti SupplierInvoiceCostDTOGet
+                var items = JsonSerializer.Deserialize<List<SupplierInvoiceCost>>(json,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return items;
+
+            }
+            return new List<SupplierInvoiceCost>();
+        }
+
+        public SupplierInvoiceCost GetById(int id)
+        {
+            ClientAPI client = new ClientAPI();
+            HttpResponseMessage response = client.GetClient().GetAsync(client.GetBaseUri() + $"supplier-invoice-cost/{id}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                // Leggere il contenuto della risposta
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                // Deserializzare la risposta JSON in una lista di oggetti SupplierInvoiceCostDTOGet
+                var items = JsonSerializer.Deserialize<SupplierInvoiceCost>(json,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return items;
+
+            }
+            string errorMessage = response.Content.ReadAsStringAsync().Result;
+            throw new Exception($"Error getting supplier invoice-cost: {errorMessage}");
+        }
+
+        public int Count(SupplierInvoiceCostFilter filter)
+        {
+            ClientAPI client = new ClientAPI();
+            var queryParameters = new List<string>();
+
+            if (filter.SupplierInvoiceId != null)
+                queryParameters.Add($"SupplierInvoiceId={filter.SupplierInvoiceId}");
+
+            if (filter.Cost != null)
+                queryParameters.Add($"Cost={filter.Cost}");
+
+            if (filter.Quantity != null)
+                queryParameters.Add($"Quantity={filter.Quantity}");
+
+            if (filter.page != null)
+                queryParameters.Add($"page={filter.page}");
+
+            string queryString = queryParameters.Any() ? "?" + string.Join("&", queryParameters) : string.Empty;
+            HttpResponseMessage response = client.GetClient().GetAsync(client.GetBaseUri() + "supplier-invoice-cost/count" + queryString).Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                // Leggere il contenuto della risposta
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                int count = JsonSerializer.Deserialize<int>(json);
+                return count;
+
+
+            }
+            return 0;
+        }
+
+        public SupplierInvoiceCost Create(SupplierInvoiceCost entity)
+        {
+            string jsonContent = JsonSerializer.Serialize(entity);
+            var returnSupplierInvoiceCost = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            ClientAPI client = new ClientAPI();
+            HttpResponseMessage response = client.GetClient().PostAsync(client.GetBaseUri() + $"supplier-invoice-cost", returnSupplierInvoiceCost).Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                // Leggere il contenuto della risposta
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                // Deserializzare la risposta JSON in una lista di oggetti SupplierInvoiceCostDTOGet
+                var items = JsonSerializer.Deserialize<SupplierInvoiceCost>(json,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return items;
+
+            }
+            string errorMessage = response.Content.ReadAsStringAsync().Result;
+            throw new Exception($"Error creating supplier invoice cost: {errorMessage}");
+        }
+
+        public SupplierInvoiceCost Update(int id, SupplierInvoiceCost entity)
+        {
+            string jsonContent = JsonSerializer.Serialize(entity);
+            var returnSupplierInvoiceCost = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            ClientAPI client = new ClientAPI();
+            HttpResponseMessage response = client.GetClient().PostAsync(client.GetBaseUri() + $"supplier-invoice-cost/{id}", returnSupplierInvoiceCost).Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                // Leggere il contenuto della risposta
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                // Deserializzare la risposta JSON in una lista di oggetti SupplierInvoiceCostDTOGet
+                var items = JsonSerializer.Deserialize<SupplierInvoiceCost>(json,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return items;
+
+            }
+            string errorMessage = response.Content.ReadAsStringAsync().Result;
+            throw new Exception($"Error updating supplier invoice cost: {errorMessage}");
+        }
+
+        public SupplierInvoiceCost Delete(int id)
+        {
+            ClientAPI client = new ClientAPI();
+            HttpResponseMessage response = client.GetClient().DeleteAsync(client.GetBaseUri() + $"supplier-invoice-cost/{id}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                // Leggere il contenuto della risposta
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                // Deserializzare la risposta JSON in una lista di oggetti SupplierInvoiceCostDTOGet
+                var items = JsonSerializer.Deserialize<SupplierInvoiceCost>(json,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return items;
+
+            }
+            string errorMessage = response.Content.ReadAsStringAsync().Result;
+            throw new Exception($"Error deleting supplier invoice-cost: {errorMessage}");
+        }
+    }
+}
