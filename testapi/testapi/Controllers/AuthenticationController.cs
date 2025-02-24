0de3cced7c72963531de10a2d35f1fd1028fe85f
Catalin Grumeza (MSC Technology Italia) <catalin.grumeza@msc.com>
2025-02-24 17:58:08 +0100

    added login api

---
+++ testapi/testapi/Controllers/AuthenticationController.cs
@@ -0,0 +1,47 @@
+﻿using API.Models.DTO;
+using API.Models.Entities;
+using API.Models.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserService _authenticationService;
+        public AuthenticationController(UserService auth)
+        {
+            _authenticationService = auth;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<string>> Register(UserDTOCreate request)
+        {
+            User user;
+            try
+            {
+               user= _authenticationService.CreateUser(request);
+            }catch (Exception ex) { return BadRequest(ex.Message); }
+
+            return Ok("User Registered Successfully ");
+        }
+        //[Authorize(Roles ="Admin")]
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDTO request)
+        {
+            User user;
+            try
+            {
+            user=_authenticationService.GetUserByEmail(request.Email);
+            }catch (Exception ex) {return BadRequest(ex.Message);}
+            if (!_authenticationService.VeryfyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+                return BadRequest("Wrong Password!");
+            string token=_authenticationService.CreateToken(user);
+            return Ok(token);
+        }
+        
+    }
+}
---
+++ testapi/testapi/Models/DTO/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Models.DTO
+{
+    public class UserDTO
+    {
+        
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+    public class UserDTOCreate:UserDTO
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
