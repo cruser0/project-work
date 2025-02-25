54135a79ed52e4f136cec1955102f3983b3df152
Catalin Grumeza (MSC Technology Italia) <catalin.grumeza@msc.com>
2025-02-25 10:43:36 +0100

    added service winform for login and register

---
+++ testapi/Winform/Services/UserService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Winform.Entities;
+using Winform.Entities.DTO;
+
+namespace Winform.Services
+{
+    internal class UserService
+    {
+        public string Register(UserDTOCreate user)
+        {
+            string jsonContent = JsonSerializer.Serialize(user);
+            var returnUser = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            ClientAPI client = new ClientAPI();
+            HttpResponseMessage response = client.GetClient().PostAsync(client.GetBaseUri() + $"register", returnUser).Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                // Leggere il contenuto della risposta
+                string json = response.Content.ReadAsStringAsync().Result;
+                return json;
+
+            }
+            string errorMessage = response.Content.ReadAsStringAsync().Result;
+            throw new Exception($"Error creating customer invoice cost: {errorMessage}");
+        }
+        public UserAccessTemp Login(UserDTO entity)
+        {
+            string jsonContent = JsonSerializer.Serialize(entity);
+            var returnCustomerInvoiceCost = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            ClientAPI client = new ClientAPI();
+            HttpResponseMessage response = client.GetClient().PostAsync(client.GetBaseUri() + $"login", returnCustomerInvoiceCost).Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                // Leggere il contenuto della risposta
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                // Deserializzare la risposta JSON in una lista di oggetti CustomerInvoiceCostDTOGet
+                var items = JsonSerializer.Deserialize<UserAccessTemp>(json,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                UserAccessInfo.Email = items.Email;
+                UserAccessInfo.Name= items.Name;
+                UserAccessInfo.Token= items.Token;
+                UserAccessInfo.LastName= items.LastName;
+                return items;
+
+            }
+            string errorMessage = response.Content.ReadAsStringAsync().Result;
+            throw new Exception($"Error creating customer invoice cost: {errorMessage}");
+        }
+    }
+}
