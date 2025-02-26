3f0bec9dc491520e5a502cdd3191eafd0f1ba1fd
Federico Giuliano (External) <E-federico.giuliano@msc.com>
2025-02-26 15:06:08 +0100

    inizio dell'user form

--- testapi/testapi/Models/Filters/UserFilter.cs
+++ testapi/testapi/Models/Filters/UserFilter.cs
@@ -2,11 +2,11 @@
 {
     public class UserFilter
     {
-        public int UserID { get; set; }
+        public int? UserID { get; set; }
         public string? Name { get; set; }
         public string? LastName { get; set; }
         public string? Email { get; set; }
-        public List<string>? Roles { get; set; }=new List<string>();
+        public List<string>? Roles { get; set; } = new List<string>();
         public int? page { get; set; }
     }
 }
