ab600bcbe9ca7dcf1934046e6fa5e775245ccc99
Catalin Grumeza (MSC Technology Italia) <catalin.grumeza@msc.com>
2025-03-04 12:17:54 +0100

    added user profile

---
+++ testapi/Winform/Forms/UserProfileForm.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace Winform.Forms
+{
+    partial class UserProfileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BottomPanel = new System.Windows.Forms.Panel();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.LeftPanel = new System.Windows.Forms.Panel();
+            this.FlowPanelRoles = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.RoleLbl = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.RightPanel = new System.Windows.Forms.Panel();
+            this.CenterPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.PasswordConfirmLbl = new System.Windows.Forms.Label();
+            this.ConfirmPasswordTxt = new System.Windows.Forms.TextBox();
+            this.UserIDTxt = new System.Windows.Forms.TextBox();
+            this.PasswordSeeBtn = new System.Windows.Forms.Button();
+            this.PasswordLbl = new System.Windows.Forms.Label();
+            this.PasswordTxt = new System.Windows.Forms.TextBox();
+            this.UserEmailLbl = new System.Windows.Forms.Label();
+            this.UserEmailTxt = new System.Windows.Forms.TextBox();
+            this.UserLastNameLbl = new System.Windows.Forms.Label();
+            this.UserLastNameTxt = new System.Windows.Forms.TextBox();
+            this.UserNameLbl = new System.Windows.Forms.Label();
+            this.UserNameTxt = new System.Windows.Forms.TextBox();
+            this.UserIDLbl = new System.Windows.Forms.Label();
+            this.LeftPanel.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.CenterPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BottomPanel
+            // 
+            this.BottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomPanel.Location = new System.Drawing.Point(0, 494);
+            this.BottomPanel.Name = "BottomPanel";
+            this.BottomPanel.Size = new System.Drawing.Size(1046, 100);
+            this.BottomPanel.TabIndex = 0;
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1046, 100);
+            this.TopPanel.TabIndex = 1;
+            // 
+            // LeftPanel
+            // 
+            this.LeftPanel.Controls.Add(this.FlowPanelRoles);
+            this.LeftPanel.Controls.Add(this.panel3);
+            this.LeftPanel.Controls.Add(this.panel2);
+            this.LeftPanel.Controls.Add(this.panel1);
+            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftPanel.Location = new System.Drawing.Point(0, 100);
+            this.LeftPanel.Name = "LeftPanel";
+            this.LeftPanel.Size = new System.Drawing.Size(200, 394);
+            this.LeftPanel.TabIndex = 2;
+            // 
+            // FlowPanelRoles
+            // 
+            this.FlowPanelRoles.AutoScroll = true;
+            this.FlowPanelRoles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FlowPanelRoles.Location = new System.Drawing.Point(10, 111);
+            this.FlowPanelRoles.Name = "FlowPanelRoles";
+            this.FlowPanelRoles.Size = new System.Drawing.Size(180, 283);
+            this.FlowPanelRoles.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.RoleLbl);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(10, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(180, 111);
+            this.panel3.TabIndex = 4;
+            // 
+            // RoleLbl
+            // 
+            this.RoleLbl.AutoSize = true;
+            this.RoleLbl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.RoleLbl.Location = new System.Drawing.Point(0, 96);
+            this.RoleLbl.Name = "RoleLbl";
+            this.RoleLbl.Size = new System.Drawing.Size(40, 15);
+            this.RoleLbl.TabIndex = 0;
+            this.RoleLbl.Text = "Roles:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Winform.Properties.Resources.cropped_logo_removebg_preview_50x50;
+            this.pictureBox1.Location = new System.Drawing.Point(52, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 53);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 394);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(190, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 394);
+            this.panel1.TabIndex = 1;
+            // 
+            // RightPanel
+            // 
+            this.RightPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightPanel.Location = new System.Drawing.Point(846, 100);
+            this.RightPanel.Name = "RightPanel";
+            this.RightPanel.Size = new System.Drawing.Size(200, 394);
+            this.RightPanel.TabIndex = 3;
+            // 
+            // CenterPanel
+            // 
+            this.CenterPanel.Controls.Add(this.button1);
+            this.CenterPanel.Controls.Add(this.checkBox1);
+            this.CenterPanel.Controls.Add(this.PasswordConfirmLbl);
+            this.CenterPanel.Controls.Add(this.ConfirmPasswordTxt);
+            this.CenterPanel.Controls.Add(this.UserIDTxt);
+            this.CenterPanel.Controls.Add(this.PasswordSeeBtn);
+            this.CenterPanel.Controls.Add(this.PasswordLbl);
+            this.CenterPanel.Controls.Add(this.PasswordTxt);
+            this.CenterPanel.Controls.Add(this.UserEmailLbl);
+            this.CenterPanel.Controls.Add(this.UserEmailTxt);
+            this.CenterPanel.Controls.Add(this.UserLastNameLbl);
+            this.CenterPanel.Controls.Add(this.UserLastNameTxt);
+            this.CenterPanel.Controls.Add(this.UserNameLbl);
+            this.CenterPanel.Controls.Add(this.UserNameTxt);
+            this.CenterPanel.Controls.Add(this.UserIDLbl);
+            this.CenterPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CenterPanel.Location = new System.Drawing.Point(200, 100);
+            this.CenterPanel.Name = "CenterPanel";
+            this.CenterPanel.Size = new System.Drawing.Size(646, 394);
+            this.CenterPanel.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(264, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 54;
+            this.button1.Text = "Save Changes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SaveEditCustomerBtn_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(212, 375);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(46, 19);
+            this.checkBox1.TabIndex = 53;
+            this.checkBox1.Text = "Edit";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // PasswordConfirmLbl
+            // 
+            this.PasswordConfirmLbl.AutoSize = true;
+            this.PasswordConfirmLbl.Enabled = false;
+            this.PasswordConfirmLbl.Location = new System.Drawing.Point(196, 317);
+            this.PasswordConfirmLbl.Name = "PasswordConfirmLbl";
+            this.PasswordConfirmLbl.Size = new System.Drawing.Size(104, 15);
+            this.PasswordConfirmLbl.TabIndex = 51;
+            this.PasswordConfirmLbl.Text = "Confirm Password";
+            this.PasswordConfirmLbl.Visible = false;
+            // 
+            // ConfirmPasswordTxt
+            // 
+            this.ConfirmPasswordTxt.Location = new System.Drawing.Point(195, 335);
+            this.ConfirmPasswordTxt.Name = "ConfirmPasswordTxt";
+            this.ConfirmPasswordTxt.PasswordChar = '*';
+            this.ConfirmPasswordTxt.PlaceholderText = "************";
+            this.ConfirmPasswordTxt.Size = new System.Drawing.Size(194, 23);
+            this.ConfirmPasswordTxt.TabIndex = 50;
+            this.ConfirmPasswordTxt.Visible = false;
+            // 
+            // UserIDTxt
+            // 
+            this.UserIDTxt.Enabled = false;
+            this.UserIDTxt.Location = new System.Drawing.Point(195, 96);
+            this.UserIDTxt.Margin = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.UserIDTxt.Name = "UserIDTxt";
+            this.UserIDTxt.Size = new System.Drawing.Size(194, 23);
+            this.UserIDTxt.TabIndex = 49;
+            // 
+            // PasswordSeeBtn
+            // 
+            this.PasswordSeeBtn.FlatAppearance.BorderSize = 0;
+            this.PasswordSeeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PasswordSeeBtn.Image = global::Winform.Properties.Resources.eye_ri;
+            this.PasswordSeeBtn.Location = new System.Drawing.Point(395, 287);
+            this.PasswordSeeBtn.Name = "PasswordSeeBtn";
+            this.PasswordSeeBtn.Size = new System.Drawing.Size(25, 23);
+            this.PasswordSeeBtn.TabIndex = 48;
+            this.PasswordSeeBtn.UseVisualStyleBackColor = true;
+            this.PasswordSeeBtn.Visible = false;
+            this.PasswordSeeBtn.Click += new System.EventHandler(this.PasswordSeeBtn_Click);
+            // 
+            // PasswordLbl
+            // 
+            this.PasswordLbl.AutoSize = true;
+            this.PasswordLbl.Enabled = false;
+            this.PasswordLbl.Location = new System.Drawing.Point(196, 270);
+            this.PasswordLbl.Name = "PasswordLbl";
+            this.PasswordLbl.Size = new System.Drawing.Size(57, 15);
+            this.PasswordLbl.TabIndex = 47;
+            this.PasswordLbl.Text = "Password";
+            // 
+            // PasswordTxt
+            // 
+            this.PasswordTxt.Enabled = false;
+            this.PasswordTxt.Location = new System.Drawing.Point(195, 288);
+            this.PasswordTxt.Name = "PasswordTxt";
+            this.PasswordTxt.PasswordChar = '*';
+            this.PasswordTxt.PlaceholderText = "************";
+            this.PasswordTxt.Size = new System.Drawing.Size(194, 23);
+            this.PasswordTxt.TabIndex = 46;
+            // 
+            // UserEmailLbl
+            // 
+            this.UserEmailLbl.AutoSize = true;
+            this.UserEmailLbl.Location = new System.Drawing.Point(197, 217);
+            this.UserEmailLbl.Name = "UserEmailLbl";
+            this.UserEmailLbl.Size = new System.Drawing.Size(36, 15);
+            this.UserEmailLbl.TabIndex = 45;
+            this.UserEmailLbl.Text = "Email";
+            // 
+            // UserEmailTxt
+            // 
+            this.UserEmailTxt.Enabled = false;
+            this.UserEmailTxt.Location = new System.Drawing.Point(195, 235);
+            this.UserEmailTxt.Name = "UserEmailTxt";
+            this.UserEmailTxt.Size = new System.Drawing.Size(194, 23);
+            this.UserEmailTxt.TabIndex = 44;
+            // 
+            // UserLastNameLbl
+            // 
+            this.UserLastNameLbl.AutoSize = true;
+            this.UserLastNameLbl.Location = new System.Drawing.Point(196, 171);
+            this.UserLastNameLbl.Name = "UserLastNameLbl";
+            this.UserLastNameLbl.Size = new System.Drawing.Size(58, 15);
+            this.UserLastNameLbl.TabIndex = 43;
+            this.UserLastNameLbl.Text = "Lastname";
+            // 
+            // UserLastNameTxt
+            // 
+            this.UserLastNameTxt.Enabled = false;
+            this.UserLastNameTxt.Location = new System.Drawing.Point(195, 189);
+            this.UserLastNameTxt.Name = "UserLastNameTxt";
+            this.UserLastNameTxt.Size = new System.Drawing.Size(194, 23);
+            this.UserLastNameTxt.TabIndex = 42;
+            // 
+            // UserNameLbl
+            // 
+            this.UserNameLbl.AutoSize = true;
+            this.UserNameLbl.Location = new System.Drawing.Point(196, 124);
+            this.UserNameLbl.Name = "UserNameLbl";
+            this.UserNameLbl.Size = new System.Drawing.Size(39, 15);
+            this.UserNameLbl.TabIndex = 41;
+            this.UserNameLbl.Text = "Name";
+            // 
+            // UserNameTxt
+            // 
+            this.UserNameTxt.Enabled = false;
+            this.UserNameTxt.Location = new System.Drawing.Point(195, 142);
+            this.UserNameTxt.Name = "UserNameTxt";
+            this.UserNameTxt.Size = new System.Drawing.Size(194, 23);
+            this.UserNameTxt.TabIndex = 40;
+            // 
+            // UserIDLbl
+            // 
+            this.UserIDLbl.AutoSize = true;
+            this.UserIDLbl.Location = new System.Drawing.Point(196, 78);
+            this.UserIDLbl.Name = "UserIDLbl";
+            this.UserIDLbl.Size = new System.Drawing.Size(44, 15);
+            this.UserIDLbl.TabIndex = 39;
+            this.UserIDLbl.Text = "User ID";
+            // 
+            // UserProfileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 594);
+            this.Controls.Add(this.CenterPanel);
+            this.Controls.Add(this.RightPanel);
+            this.Controls.Add(this.LeftPanel);
+            this.Controls.Add(this.TopPanel);
+            this.Controls.Add(this.BottomPanel);
+            this.Name = "UserProfileForm";
+            this.Text = "UserProfileForm";
+            this.Load += new System.EventHandler(this.UserProfileForm_Load);
+            this.LeftPanel.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.CenterPanel.ResumeLayout(false);
+            this.CenterPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel BottomPanel;
+        private Panel TopPanel;
+        private Panel LeftPanel;
+        private Panel RightPanel;
+        private Label RoleLbl;
+        private PictureBox pictureBox1;
+        private Panel CenterPanel;
+        private FlowLayoutPanel FlowPanelRoles;
+        private Panel panel2;
+        private Panel panel1;
+        private Panel panel3;
+        private TextBox UserIDTxt;
+        private Label PasswordLbl;
+        private Label UserEmailLbl;
+        private TextBox UserEmailTxt;
+        private Label UserLastNameLbl;
+        private TextBox UserLastNameTxt;
+        private Label UserNameLbl;
+        private TextBox UserNameTxt;
+        private Label UserIDLbl;
+        private TextBox PasswordTxt;
+        private Label PasswordConfirmLbl;
+        private TextBox ConfirmPasswordTxt;
+        private Button PasswordSeeBtn;
+        private Button button1;
+        private CheckBox checkBox1;
+    }
+}
---
+++ testapi/Winform/Forms/UserProfileForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Winform.Entities;
+using Winform.Entities.DTO;
+using Winform.Services;
+
+namespace Winform.Forms
+{
+    public partial class UserProfileForm : Form
+    {
+        UserService userService;
+        public UserProfileForm()
+        {
+            userService = new UserService();
+            InitializeComponent();
+        }
+
+        private void UserProfileForm_Load(object sender, EventArgs e)
+        {
+            foreach(string role in UserAccessInfo.Role)
+            {
+                Label lbl=new Label();
+                lbl.Text=role;
+                FlowPanelRoles.Controls.Add(lbl);
+            }
+            UserRoleDTO user = userService.GetById(UserAccessInfo.RefreshUserID);
+            UserIDTxt.Text = user.UserID.ToString();
+            UserNameTxt.Text = user.Name;
+            UserLastNameTxt.Text = user.LastName;
+            UserEmailTxt.Text = user.Email;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            UserNameTxt.Enabled = checkBox1.Checked;
+            UserLastNameTxt.Enabled = checkBox1.Checked;
+            PasswordTxt.Enabled = checkBox1.Checked;
+            PasswordSeeBtn.Visible = checkBox1.Checked;
+            ConfirmPasswordTxt.Visible = checkBox1.Checked;
+            PasswordConfirmLbl.Visible = checkBox1.Checked;
+            button1.Enabled = checkBox1.Checked;
+        }
+
+        private void SaveEditCustomerBtn_Click(object sender, EventArgs e)
+        {
+            if (PasswordTxt.Text.Equals(ConfirmPasswordTxt.Text))
+            {
+
+
+                UserDTOEdit si = new UserDTOEdit
+                {
+                    LastName = UserLastNameTxt.Text,
+                    Name = UserNameTxt.Text,
+                    Password = !string.IsNullOrEmpty(PasswordTxt.Text) ? PasswordTxt.Text : null,
+                };
+                try
+                {
+                    userService.Update(int.Parse(UserIDTxt.Text), si);
+                    MessageBox.Show("User updated successfully!");
+
+                    this.Close();
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+            }
+            else
+                MessageBox.Show("The passwords don't match");
+        }
+        private void PasswordSeeBtn_Click(object sender, EventArgs e)
+        {
+            if (PasswordTxt.PasswordChar.Equals('*'))
+            {
+                PasswordTxt.PasswordChar = default(char);
+                ConfirmPasswordTxt.PasswordChar = default(char);
+
+            }
+            else
+            {
+                PasswordTxt.PasswordChar = '*';
+                ConfirmPasswordTxt.PasswordChar = '*';
+            }
+        }
+    }
+}
