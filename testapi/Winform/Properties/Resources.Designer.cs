8c54cdd0efe17b2c87be237b31eeed7f8dfe631e
Catalin Grumeza <catalin.grumeza@msc.com>
2025-03-06 14:19:14 +0100

    added user control for star

--- testapi/Winform/Properties/Resources.Designer.cs
+++ testapi/Winform/Properties/Resources.Designer.cs
@@ -123,6 +123,26 @@ namespace Winform.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap fav_color_no_bg {
+            get {
+                object obj = ResourceManager.GetObject("fav_color_no_bg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fav_no_bg {
+            get {
+                object obj = ResourceManager.GetObject("fav_no_bg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Invoice_Icon {
             get {
                 object obj = ResourceManager.GetObject("Invoice_Icon", resourceCulture);
