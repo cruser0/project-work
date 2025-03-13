6fdbe92077e3ac8e72f9707cb7209505d1168027
Federico Giuliano (External) <E-federico.giuliano@msc.com>
2025-03-13 17:30:24 +0100

    new reports

--- testapi/WinformDotNetFramework/Forms/GraphTestForm.Designer.cs
+++ testapi/WinformDotNetFramework/Forms/GraphTestForm.Designer.cs
@@ -28,22 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(438, 42);
+            this.button1.Location = new System.Drawing.Point(6, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(155, 64);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,71 +46,35 @@
             // 
             // reportViewer1
             // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.reportViewer1.LocalReport.EnableExternalImages = true;
-            this.reportViewer1.LocalReport.ReportEmbeddedResource = "WinformDotNetFramework.Report2.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(40, 90);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "WinformDotNetFramework.Reports.ReportSales.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(705, 615);
+            this.reportViewer1.Size = new System.Drawing.Size(751, 770);
             this.reportViewer1.TabIndex = 8;
             // 
-            // chart1
+            // panel1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(804, 90);
-            this.chart1.Name = "chart1";
-            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
-            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
-        System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(10)))), ((int)(((byte)(14))))),
-        System.Drawing.Color.OrangeRed,
-        System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(69)))), ((int)(((byte)(235))))),
-        System.Drawing.Color.Yellow};
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(300, 300);
-            this.chart1.TabIndex = 9;
-            this.chart1.Text = "chart1";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(550, 41);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(663, 42);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(751, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(386, 770);
+            this.panel1.TabIndex = 10;
             // 
             // GraphTestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1137, 705);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.chart1);
+            this.ClientSize = new System.Drawing.Size(1137, 770);
             this.Controls.Add(this.reportViewer1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Name = "GraphTestForm";
             this.Text = "GraphTestForm";
             this.Load += new System.EventHandler(this.GraphTestForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -123,8 +82,6 @@
         #endregion
         private System.Windows.Forms.Button button1;
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- testapi/WinformDotNetFramework/Forms/GraphTestForm.cs
+++ testapi/WinformDotNetFramework/Forms/GraphTestForm.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.Reporting.WinForms;
 using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
+using WinformDotNetFramework.Procedures;
 using WinformDotNetFramework.Services;
 
 namespace WinformDotNetFramework.Forms
@@ -10,7 +10,8 @@ namespace WinformDotNetFramework.Forms
     public partial class GraphTestForm : Form
     {
         ProceduresService _procedureService;
-        CustomerService _customerService;
+        private List<ClassifySalesByProfit> orderDetailsData = null;
+
         public GraphTestForm()
         {
             InitializeComponent();
@@ -19,115 +20,39 @@ namespace WinformDotNetFramework.Forms
         private void Init()
         {
             _procedureService = new ProceduresService();
-            _customerService = new CustomerService();
         }
 
         private async void GraphTestForm_Load(object sender, EventArgs e)
         {
-            //var data = await _customerService.GetAll(new Entities.Filters.CustomerFilter());
+            reportViewer1.ProcessingMode = ProcessingMode.Local;
 
-            var data = await _procedureService.GetClassifySalesByProfit(new Entities.Filters.ClassifySalesByProfitFilter());
-            var data2 = await _procedureService.SaleMarginCount(new Entities.Filters.ClassifySalesByProfitFilter());
+            reportViewer1.LocalReport.SubreportProcessing +=
+                        new SubreportProcessingEventHandler(DemoSubreportProcessingEventHandler);
+
+            orderDetailsData = await _procedureService.GetClassifySalesByProfit(new Entities.Filters.ClassifySalesByProfitFilter());
 
             ReportDataSource dataSource = new ReportDataSource()
             {
-                Name = "DataSet1",
-                Value = data
+                Name = "SaleByProfit",
+                Value = orderDetailsData
             };
-
-            ReportDataSource dataSource2 = new ReportDataSource()
-            {
-                Name = "DataSet2",
-                Value = data2
-            };
-
             reportViewer1.LocalReport.DataSources.Add(dataSource);
-            reportViewer1.LocalReport.DataSources.Add(dataSource2);
-
-
-
-
-            chart1.Series[0].Points.Clear();
-
-            foreach (var item in data2)
-            {
-                chart1.Series[0].Points.AddXY(item.Key, item.Value);
-
-            }
-
-            reportViewer1.LocalReport.DataSources.Add(dataSource);
-            reportViewer1.LocalReport.DataSources.Add(dataSource2);
             reportViewer1.RefreshReport();
-
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
 
-            if (reportViewer1.LocalReport.ReportEmbeddedResource == "WinformDotNetFramework.Report2.rdlc")
-            {
-                chart1.SaveImage("Chart1.bmp", ChartImageFormat.Bmp);
-
-
-                ReportParameter ImageLink = new ReportParameter("ImageLink");
-
-                string relativePath = "Chart1.bmp";
-                string fullPath = Path.GetFullPath(relativePath);
-                Uri fileUri = new Uri(fullPath, UriKind.Absolute);
-                ImageLink.Values.Add(fileUri.AbsoluteUri);
-                reportViewer1.LocalReport.SetParameters(ImageLink);
-                reportViewer1.RefreshReport();
-            }
         }
 
-        private async void button2_Click(object sender, EventArgs e)
+        void DemoSubreportProcessingEventHandler(object sender, SubreportProcessingEventArgs e)
         {
-            reportViewer1.Refresh();
-            if (reportViewer1.LocalReport.ReportEmbeddedResource == "WinformDotNetFramework.Report2.rdlc")
-            {
-
-                reportViewer1.LocalReport.ReportEmbeddedResource = "WinformDotNetFramework.Report1.rdlc";
-
-                var data = await _customerService.GetAll(new Entities.Filters.CustomerFilter());
-
-                ReportDataSource dataSource = new ReportDataSource()
-                {
-                    Name = "Customers",
-                    Value = data
-                };
-
-                reportViewer1.LocalReport.DataSources.Add(dataSource);
-            }
-            else
-            {
-                reportViewer1.LocalReport.ReportEmbeddedResource = "WinformDotNetFramework.Report2.rdlc";
-                var data = await _procedureService.GetClassifySalesByProfit(new Entities.Filters.ClassifySalesByProfitFilter());
-                var data2 = await _procedureService.SaleMarginCount(new Entities.Filters.ClassifySalesByProfitFilter());
-
-                ReportDataSource dataSource = new ReportDataSource()
-                {
-                    Name = "DataSet1",
-                    Value = data
-                };
-
-                ReportDataSource dataSource2 = new ReportDataSource()
-                {
-                    Name = "DataSet2",
-                    Value = data2
-                };
-
-                reportViewer1.LocalReport.DataSources.Add(dataSource);
-                reportViewer1.LocalReport.DataSources.Add(dataSource2);
-            }
-
-            reportViewer1.Refresh();
+            e.DataSources.Add(new ReportDataSource("SaleStatusChart", orderDetailsData));
+            e.DataSources.Add(new ReportDataSource("SaleProfit", orderDetailsData));
+            e.DataSources.Add(new ReportDataSource("SaleProfitTemporal", orderDetailsData));
         }
 
-        int count = 0;
-        private void button3_Click(object sender, EventArgs e)
-        {
-            chart1.Series[0].ChartType = (SeriesChartType)(count % 8);
-            count++;
-        }
+
+
     }
 }
