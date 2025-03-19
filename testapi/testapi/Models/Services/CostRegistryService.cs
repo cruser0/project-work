df09cecaa0c23c2fd04416a761080a23963814e3
Federico Giuliano (External) <E-federico.giuliano@msc.com>
2025-03-19 14:01:28 +0100

    creati services per ost registry e status

---
+++ testapi/testapi/Models/Services/CostRegistryService.cs
@@ -0,0 +1,18 @@
+﻿using API.Models.Entities;
+
+namespace API.Models.Services
+{
+    public class CostRegistryService
+    {
+        private readonly Progetto_FormativoContext _context;
+        public CostRegistryService(Progetto_FormativoContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public CostRegistry GetCostRegistryByCode(string name)
+        {
+            return _context.CostRegistries.Where(x => x.CostRegistryUniqueCode.Equals(name)).First();
+        }
+    }
+}
