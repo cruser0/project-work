e323b227cb1343ab44090779b4b992b072098169
Catalin Grumeza <catalin.grumeza@msc.com>
2025-03-19 12:44:25 +0100

    finished seeders

---
+++ testapi/testapi/Models/Configuration/RegistryCostConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.Models.Configuration
+{
+    public class RegistryCostConfiguration : IEntityTypeConfiguration<CostRegistry>
+    {
+        public void Configure(EntityTypeBuilder<CostRegistry> builder)
+        {
+            builder.HasData(new CostRegistry() { CostRegistryID = 1, CostRegistryName = "Generic", CostRegistryPrice = 1, CostRegistryQuantity = 1, CostRegistryUniqueCode = "GNC" });
+        }
+    }
+}
