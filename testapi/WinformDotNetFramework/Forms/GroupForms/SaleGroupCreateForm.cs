c441c7e6eecce39e3cedb48ce43cc11dddb8c301
Catalin Grumeza <catalin.grumeza@msc.com>
2025-03-25 17:50:38 +0100

    added group to sale

---
+++ testapi/WinformDotNetFramework/Forms/GroupForms/SaleGroupCreateForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinformDotNetFramework.Forms.AddForms;
+
+namespace WinformDotNetFramework.Forms.GroupForms
+{
+    public partial class SaleGroupCreateForm : CreateSaleForm
+    {
+        public SaleGroupCreateForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
