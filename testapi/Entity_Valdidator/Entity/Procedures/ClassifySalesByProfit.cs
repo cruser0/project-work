a712eecc9b731d8850d6c324c083c9b82a9f6189
Federico Giuliano (External) <E-federico.giuliano@msc.com>
2025-03-31 09:01:40 +0200

    finished fixing namespaces

--- testapi/Entity_Valdidator/Entity/Procedures/ClassifySalesByProfit.cs
+++ testapi/Entity_Valdidator/Entity/Procedures/ClassifySalesByProfit.cs
@@ -1,22 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
 
-namespace API.Models.Procedures
+namespace Entity_Validator.Entity.Procedures
 {
     public class ClassifySalesByProfit
     {
         public int? SaleID { get; set; }
         public decimal? TotalRevenue { get; set; }
-        public string? BoLNumber { get; set; }
-        public string? BookingNumber { get; set; }
+        public string BoLNumber { get; set; }
+        public string BookingNumber { get; set; }
         public DateTime? SaleDate { get; set; }
-        public string? Status { get; set; }
-        public string? CustomerName { get; set; }
-        public string? Country { get; set; }
-        [Column("Region")]
-        public string? CountryRegion { get; set; }
+        public string Status { get; set; }
+        public string CustomerName { get; set; }
+        public string Country { get; set; }
+        public string CountryRegion { get; set; }
         public decimal? TotalSpent { get; set; }
         public decimal? Profit { get; set; }
-        public string? SaleMargins { get; set; }
+        public string SaleMargins { get; set; }
 
     }
 }
