cf7d5945aecd6cfe6103c4a755356cc92c09e686
Federico Giuliano (External) <E-federico.giuliano@msc.com>
2025-04-01 10:00:54 +0200

    better test to show mistakes

--- testapi/WinformDotNetFramework/Form1.Designer.cs
+++ testapi/WinformDotNetFramework/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.CustomerInvoiceCode = new WinformDotNetFramework.RedTextBox();
             this.CostRegistryCode = new WinformDotNetFramework.RedTextBox();
             this.name = new WinformDotNetFramework.RedTextBox();
@@ -53,16 +52,6 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(104, 80);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // CustomerInvoiceCode
             // 
@@ -183,7 +172,6 @@
             this.Controls.Add(this.Quantity);
             this.Controls.Add(this.Cost);
             this.Controls.Add(this.CustomerInvoiceId);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -195,7 +183,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private RedTextBox CustomerInvoiceId;
         private RedTextBox Cost;
         private RedTextBox name;
--- testapi/WinformDotNetFramework/Form1.cs
+++ testapi/WinformDotNetFramework/Form1.cs
@@ -12,6 +12,125 @@ namespace WinformDotNetFramework
     public partial class Form1 : Form
     {
         public HashSet<RedTextBox> Wrongtextboxes = new HashSet<RedTextBox>();
+
+        // Lista di 10 CustomerInvoiceCostDTO con 9 che contengono errori
+        List<CustomerInvoiceCostDTO> dtoList = new List<CustomerInvoiceCostDTO>
+{
+    // 1. DTO valido
+    new CustomerInvoiceCostDTO
+    {
+        CustomerInvoiceId = 1,
+        Cost = 100.50m,
+        Quantity = 5,
+        Name = "Valid Invoice Item",
+        CostRegistryCode = "REG-12345",
+        CustomerInvoiceCode = "INV-2025-001",
+        IsPost = true
+    },
+    
+    // 2. Errore: CustomerInvoiceId mancante (null)
+    new CustomerInvoiceCostDTO
+    {
+        CustomerInvoiceId = null,
+        Cost = 75.99m,
+        Quantity = 3,
+        Name = "Office Supplies",
+        CostRegistryCode = "REG-1001",
+        CustomerInvoiceCode = "INV-2025-002",
+        IsPost = true
+    },
+    
+    // 3. Errore: Cost negativo (viola il constraint Range)
+    new CustomerInvoiceCostDTO
+    {
+        CustomerInvoiceId = 3,
+        Cost = -50.00m,
+        Quantity = 2,
+        Name = "Software License",
+        CostRegistryCode = "REG-2002",
+        CustomerInvoiceCode = "INV-2025-003",
+        IsPost = true
+    },
+    
+    // 4. Errore: Quantity zero (viola il constraint Range)
+    new CustomerInvoiceCostDTO
+    {
+        CustomerInvoiceId = 4,
+        Cost = 199.99m,
+        Quantity = 0,
+        Name = "Hardware Maintenance",
+        CostRegistryCode = "REG-3003",
+        CustomerInvoiceCode = "INV-2025-004",
+        IsPost = true
+    },
+    
+    // 5. Errore: Name troppo lungo (eccede MaxLength)
+    new CustomerInvoiceCostDTO
+    {
+        CustomerInvoiceId = 5,
+        Cost = 349.50m,
+        Quantity = 1,
+        Name = "This name is intentionally very long to exceed the MaxLength attribute of 100 characters which is set on the Name property of the DTO class and will cause validation to fail",
+        CostRegistryCode = "REG-4004",
+        CustomerInvoiceCode = "INV-2025-005",
+        IsPost = true
+    },
+    
+    // 6. Errore: CostRegistryCode troppo lungo (eccede MaxLength)
+    new CustomerInvoiceCostDTO
+    {
+        CustomerInvoiceId = 6,
+        Cost = 89.99m,
+        Quantity = 10,
+        Name = "Monthly Support",
+        CostRegistryCode = "REG-5005-EXTRA-LONG-CODE-THAT-EXCEEDS-THE-MAXIMUM-LENGTH-OF-100-CHARACTERS-SPECIFIED-IN-THE-CONSTRAINTS-OF-THE-CUSTOMER-INVOICE-COST-DTO-CLASS",
+        CustomerInvoiceCode = "INV-2025-006",
+        IsPost = true
+    },
+    
+    // 7. Errore: CustomerInvoiceCode troppo lungo (eccede MaxLength)
+    new CustomerInvoiceCostDTO
+    {
+        CustomerInvoiceId = 7,
+        Cost = 125.00m,
+        Quantity = 5,
+        Name = "Cloud Storage",
+        CostRegistryCode = "REG-6006",
+        CustomerInvoiceCode = "INV-2025-007-THIS-INVOICE-CODE-IS-INTENTIONALLY-LONGER-THAN-THE-ALLOWED-50-CHARACTERS",
+        IsPost = true
+    },
+    
+    // 8. Errore: Campi multipli mancanti (Cost e Name null)
+    new CustomerInvoiceCostDTO
+    {
+        CustomerInvoiceId = 8,
+        Cost = null,
+        Quantity = 2,
+        Name = null,
+        CostRegistryCode = "REG-7007",
+        CustomerInvoiceCode = "INV-2025-008",
+        IsPost = true
+    },
+    
+    // 9. Nessun errore: Tutte le proprietà null ma IsPost è false
+    new CustomerInvoiceCostDTO
+    {
+        CustomerInvoiceId = null,
+        Cost = null,
+        Quantity = null,
+        Name = null,
+        CostRegistryCode = null,
+        CustomerInvoiceCode = null,
+        IsPost = false
+    },
+    
+    // 10. Errore: Tutte le proprietà richieste mancanti
+    new CustomerInvoiceCostDTO
+    {
+        IsPost = true
+        // Tutte le proprietà richieste quando IsPost=true sono mancanti
+    }
+};
         public Form1()
         {
             InitializeComponent();
@@ -28,31 +147,14 @@ namespace WinformDotNetFramework
         {
             ResetLbl();
 
-            var invoiceCost = new CustomerInvoiceCostDTO
-            {
-                CustomerInvoiceId = 1, // Un esempio di ID della fattura
-                Cost = -100.50m, // Valore negativo per il costo
-                Quantity = 10, // Esempio di quantità
-                IsPost = true // Indica che si tratta di un'inserimento (Post)
-            };
+            Random random = new Random();
+            int randomIndex = random.Next(0, dtoList.Count);
+            var invoiceCost = dtoList[randomIndex];
 
             ValidateTextBoxes(invoiceCost);
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            ResetLbl();
 
-            var invoiceCost = new CustomerInvoiceCostDTO
-            {
-                CustomerInvoiceId = 1, // Un esempio di ID della fattura
-                Cost = 100.50m, // Valore negativo per il costo
-                CostRegistryCode = "COST123", // Codice di registrazione del costo
-                IsPost = false // Indica che si tratta di un'inserimento (Post)
-            };
-
-            ValidateTextBoxes(invoiceCost);
-        }
 
 
         private void ValidateTextBoxes(CustomerInvoiceCostDTO invoiceCost)
